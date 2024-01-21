70c86ec509cfec6c6891b1e090c85065c753a2f1
Maciej Brzozowski <65349652+Brzozako@users.noreply.github.com>
2024-01-21 19:15:17 +0100

    Add files via upload
    zad10

---
+++ ASKzad10.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <stdint.h>
+using namespace std;
+
+int main()
+{
+    uint16_t y = 0;
+    uint16_t a = 8;
+    uint16_t b = 0;
+
+    b = a >> 3;
+    y = a + b;
+
+    printf("C   y: %d\n", y);
+
+    
+    _asm {
+        mov ax, a;
+        mov bx, a;
+        shr bx, 3;
+        add ax, bx;
+        mov y, ax;
+
+    }printf("ASM   y: %d\n", y);
+
+}
