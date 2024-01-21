2997a72510c4c81d28da9cd5b8448a484877525c
Maciej Brzozowski <65349652+Brzozako@users.noreply.github.com>
2024-01-21 17:27:50 +0100

    Add files via upload
    zad6 petla

---
+++ ASKzad6.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <stdint.h>
+using namespace std;
+
+int main()
+{
+    uint8_t y = 0;
+    uint8_t a = 0x12;
+    int licznik = 0;
+    int pom;
+
+    for (int i = 0; i < 8; i++) {
+        pom = a;
+        pom = pom & 1;
+        licznik = licznik + pom;
+        a = a >> 1;
+    }
+    y = 8 - licznik;
+    printf("C   y: %d\n", y);
+
+    y = 0;
+    a = 0x12;
+
+    _asm {
+        mov bl, 0;
+        mov al, a;
+    petla:
+        mov cl, al;
+        and cl, 1;
+        add bl, cl;
+        shr al, 1;
+        jnz petla;
+
+        mov dl, 8;
+        sub dl, bl;
+        mov y, dl;
+    }printf("ASM   y: %d\n", y);
+    
+
+}
