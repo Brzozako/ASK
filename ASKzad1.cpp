894c6f45b5258121c119122cd37763b97829e8c2
Maciej Brzozowski <65349652+Brzozako@users.noreply.github.com>
2024-01-21 14:37:22 +0100

    Add files via upload
    zad1 a

---
+++ ASKzad1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    unsigned char a = 15;
+    unsigned short y;
+    y = (unsigned short)(a << 4) + (unsigned short)(a << 2);
+    printf("C   y: %d\n", y);
+    _asm {
+        xor eax, eax
+        xor ebx, ebx
+        mov al, a
+        shl ax, 4
+        mov bl, a
+        shl bx, 2
+        add ax, bx
+        mov y, ax
+    }printf("ASM    y: %d\n", y);
+
+
+}
