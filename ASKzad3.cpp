4cd4cec5712e8b7651f1beff67ed4377a1b2329c
Maciej Brzozowski <65349652+Brzozako@users.noreply.github.com>
2024-01-21 14:53:53 +0100

    Add files via upload
    zad3 a

---
+++ ASKzad3.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    unsigned char a = 0x32; //a = 0011 0010
+    bool y;
+    if (a == 0x32)
+        y = false;
+    else
+        y = true;
+    printf("C   y: %d\n", y);
+    _asm {
+        mov a, al;
+        sub a, 0x32;
+        jnz falsz;
+        mov y, 1;
+        jmp koniec;
+
+    falsz:
+        mov y, 0;
+
+        koniec:
+    }printf("ASM   y: %d\n", y);
+}
